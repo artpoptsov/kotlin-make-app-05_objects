aace673c49299129f4b970c5284bd42ae8626129
Artem Poptsov <adpoptsov@gmail.com>
2022-03-23 23:18:51 +0500

    Restore

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -2,7 +2,7 @@ package ru.netology
 
 fun main() {
 
-    val post = Post(date = 12011987, text = "Тест", friendOnly = true, like = 32)
+    val post = Post(date = 12011987, text = "Тест", friendOnly = true, like = 32, ownerId = 21)
     val post1 = Post(date = 12021912, text = "Тест2", friendOnly = false, like = 10, ownerId = 74)
     val updatePost1 = Post(date = 10121912, text = "я тебя изменил", friendOnly = true, like = 120, id = 1, ownerId = 3)
 
